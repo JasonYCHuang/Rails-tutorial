8de2df66ad6eeb9cf2ebb51be9dc8f45f6fcfca5
JasonYCH <ychuang1981@gmail.com>
2015-07-07 23:05:41 +0200

    #update-signup-rpec

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,20 @@
+class RegistrationsController < Devise::RegistrationsController
+  def new
+    super
+  end
+
+  def create
+    super
+  end
+
+
+  protected    #redirect to show after edit user profile
+    def after_sign_up_path_for(resource)
+      user_path(resource)
+    end
+
+    def after_update_path_for(resource)
+      user_path(resource)
+    end
+  
+end
---
+++ spec/requests/user_pages_spec.rb
@@ -0,0 +1,63 @@
+describe "User pages"  do
+
+  subject { page }
+
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
+  end
+
+  describe "signup page" do
+    before { visit new_user_registration_path }
+
+    it { should have_content('Sign up') }
+    it { should have_title(full_title('Sign up')) }
+  end
+
+  describe "signup" do
+    before { visit new_user_registration_path }
+
+    let(:submit) { "Create my account" }
+    let(:fillin_signup) {
+    	fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobarfoobar"
+        fill_in "Password confirmation", with: "foobarfoobar"
+    }
+
+    describe "with valid information" do
+      before { fillin_signup }
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
+
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "after saving the user" do
+      before { fillin_signup }
+      before { click_button submit }
+
+      let(:user) { User.find_by(email: 'user@example.com') }
+
+      it { should have_title(user.name) }
+      it { should have_selector('div.alert.alert-info.alert-dismissible', text: 'Welcome') }
+    end
+
+    describe "after submission" do
+      before { click_button submit }
+
+      it { should have_title('Sign up') }
+      it { should have_content('error') }
+    end
+
+  end
+end
