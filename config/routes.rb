303e34ed03695e93de9d2bd3f4cc90930cbe44bc
JasonYCH <ychuang1981@gmail.com>
2015-07-07 14:38:30 +0200

    +Initialize-devise-basic

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   get 'about'   => 'welcomes#about'
   get 'contact' => 'welcomes#contact'
 
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
