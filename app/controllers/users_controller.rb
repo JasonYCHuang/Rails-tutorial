936d5c30352480ca01ebc213f926124700e0de33
JasonYCH <ychuang1981@gmail.com>
2015-07-08 19:13:24 +0200

    +add-users-lists

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,19 @@
 class UsersController < ApplicationController
+	before_action :authenticate_user!
+	before_action :is_correct_user!, except: [:index]		# TBD (will be updated if using pundit)
+
+	def index
+		@users = User.all
+  	end
 
 	def show
 		@user = User.find(params[:id])
 	end
+
+	private
+		def is_correct_user!
+	      @user = User.find(params[:id])
+	      redirect_to root_path, alert: "You don't have the access right!" unless @user == current_user
+	    end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(name:  "Example User",
+             email: "example@rails.org",
+             password:              "foobarfoobar",
+             password_confirmation: "foobarfoobar")
+
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "foobarfoobar"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
